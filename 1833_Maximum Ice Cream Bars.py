e1f7af0dfed2674e9ee374732f3200f8a8cc5401
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-01-07 00:14:22 +0100

    solution for 1833. Maximum Ice Cream Bars

---
+++ 1833_Maximum Ice Cream Bars.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        num = 0
+        for i in range(len(costs)):
+            if coins > 0 and coins >= costs[i]:
+                coins -= costs[i] 
+                num += 1
+        return num
