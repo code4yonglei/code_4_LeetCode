6dd315b5e20cb2d596112c488d97a9a90aefbc8d
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-01-08 22:54:05 +0100

    149 | python3

---
+++ 149. Max Points on a Line.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        points.sort()
+        dict1, max_val = collections.defaultdict(int), 0
+
+        for i in range(len(points)):
+            dict1.clear()
+            for j in range(i+1,len(points)):
+                x, y = points[j][0] - points[i][0], points[j][1] - points[i][1]
+                m = (x//math.gcd(x,y),y//math.gcd(x,y))
+                dict1[m] += 1
+                max_val = max(max_val,dict1[m])
+        return max_val + 1
