06d60d80de7e3201ca2821a541a1389914828524
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-01-08 22:57:53 +0100

    1523 | cpp

---
+++ 1523. Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int num = high - low;
+        if (low%2 == 1 || high%2 == 1)
+            return num/2 + 1; 
+        else
+            return num/2;
+    }
+};
+
