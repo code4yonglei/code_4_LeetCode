f4651766ce346df44ded96b78b94985474715dcd
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-01-13 23:18:40 +0100

    2525 | cpp

---
+++ 2525. Categorize Box According to Criteria.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string categorizeBox(int l, int w, int h, int m) {
+        long long v=(long long)l*(long long)w*(long long)h;
+        if(l>=10000||w>=10000||h>=10000||v>=pow(10,9)){
+            if (m>=100) return "Both";
+            else return "Bulky";
+        }
+        else if (m>=100) return "Heavy";
+        else return "Neither";
+    }
+};
