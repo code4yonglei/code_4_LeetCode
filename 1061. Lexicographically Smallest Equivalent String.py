7084620c238342248ee619b21bb6dd8cf4417564
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-01-14 22:55:12 +0100

    1061 | python3

---
+++ 1061. Lexicographically Smallest Equivalent String.py
@@ -0,0 +1,20 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        smallest={}
+        for x in string.ascii_lowercase:
+            smallest[x]=x
+
+        def get_small(x):
+            if smallest[x] != x:
+                smallest[x]=get_small(smallest[x])
+            return smallest[x]
+
+        for a,b in zip(s1,s2):
+            smaller=min(get_small(a),get_small(b))
+            smallest[get_small(a)]=smaller
+            smallest[get_small(b)]=smaller
+        res=[]
+        for x in baseStr:
+            res.append(get_small(x))
+        return "".join(res)
+      
