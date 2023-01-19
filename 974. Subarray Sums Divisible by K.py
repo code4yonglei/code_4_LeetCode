b8dbcbf9a76c69d0fab1d19f44c279b8d86a45ce
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-01-20 00:20:27 +0100

    974 | python3

---
+++ 974. Subarray Sums Divisible by K.py
@@ -0,0 +1,23 @@
+'''
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+A subarray is a contiguous part of an array.
+
+Example 1:
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+'''
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        dict = defaultdict(int)
+        dict[0] =1
+        output = 0
+        sum = 0
+        for n in nums:
+            sum +=n
+            m = sum%k
+            output += dict[m]
+            dict[m]+=1
+        return output
