dc40653e39ebc69f54c52dd2612e171ebf29b757
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-08-23 00:32:22 +0200

    update solutions

---
+++ python3/2828_Check_if_a_String_Is_an_Acronym_of_Words.py
@@ -0,0 +1,28 @@
+"""
+Example 1:
+Input: words = ["alice","bob","charlie"], s = "abc"
+Output: true
+Explanation: The first character in the words "alice", "bob", and "charlie" are 'a', 'b', and 'c', respectively. Hence, s = "abc" is the acronym. 
+
+Example 2:
+Input: words = ["an","apple"], s = "a"
+Output: false
+Explanation: The first character in the words "an" and "apple" are 'a' and 'a', respectively. 
+The acronym formed by concatenating these characters is "aa". 
+Hence, s = "a" is not the acronym.
+
+Example 3:
+Input: words = ["never","gonna","give","up","on","you"], s = "ngguoy"
+Output: true
+Explanation: By concatenating the first character of the words in the array, we get the string "ngguoy". 
+Hence, s = "ngguoy" is the acronym.
+"""
+
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        s_word = ""
+        for iword in words:
+            s_word += iword[0]
+        return s_word == s
+
+        # return ''.join(word[0] for word in words) == s
